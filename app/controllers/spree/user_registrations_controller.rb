545738be87110fe4a8487e54402aee6ef2cff39b
Mausam Pun <sonicmausam@gmail.com>
2025-05-21 15:13:22 +0545

    patch the controllers for spree v5

--- app/controllers/spree/user_registrations_controller.rb
+++ app/controllers/spree/user_registrations_controller.rb
@@ -1,6 +1,20 @@
 module Spree
   class UserRegistrationsController < ::Devise::RegistrationsController
-    include Spree::Storefront::DeviseConcern
+    helper_method :title
+    layout 'spree/storefront'
+
+    include Spree::Core::ControllerHelpers::Order
+    include Spree::LocaleUrls
+    include Spree::ThemeConcern
+    include Spree::AnalyticsHelper
+    include Spree::IntegrationsHelper if defined?(Spree::IntegrationsHelper)
+
+    helper 'spree/wishlist'
+    helper 'spree/currency'
+    helper 'spree/locale'
+    helper 'spree/storefront_locale'
+    helper 'spree/integrations' if defined?(Spree::IntegrationsHelper)
+    # include Spree::Storefront::DeviseConcern
 
     protected
 
